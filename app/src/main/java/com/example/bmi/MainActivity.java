07f097481928e4350301c969d9fdb6d3b8c46d81
jung915 <jung915@tc.edu.tw>
2020-05-03 19:34:41 +0800

    android basics: 28%, context and textview

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editWeight;
+    private EditText editHeight;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.main_linear);
+
+        //add as fields
+        findViews();
+    }
+
+    private void findViews() {
+        editWeight = findViewById(R.id.edit_weight);
+        editHeight = findViewById(R.id.edit_height);
+        result = findViewById(R.id.result);
+    }
+
+    //onClick event
+    public void bmi(View view) {
+
+        String w = editWeight.getText().toString();
+        String h = editHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight / (height * height);
+        //Log.d("MainActivity","BMI"+bmi);
+        Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
+        result.setText("your bmi is " + bmi);
     }
 }
