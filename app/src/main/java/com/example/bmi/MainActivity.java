6db27c87196008cf2cffd59ae9e36dfae78741e1
jung915 <jung915@tc.edu.tw>
2020-05-16 18:41:51 +0800

    android basics: 30%, AlertDialog.Builder & OnClickListener

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.bmi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +32,17 @@ public class MainActivity extends AppCompatActivity {
         editWeight = findViewById(R.id.edit_weight);
         editHeight = findViewById(R.id.edit_height);
         result = findViewById(R.id.result);
+        Button help = findViewById(R.id.help);
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("HELP")
+                        .setMessage("Body mass index (BMI) is a value derived from the mass (weight) and height of a person.")
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
     }
 
     //onClick event
@@ -42,5 +56,10 @@ public class MainActivity extends AppCompatActivity {
         //Log.d("MainActivity","BMI"+bmi);
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();
         result.setText("your bmi is " + bmi);
+        new AlertDialog.Builder(this).setTitle("BMI").setMessage("your bmi is :" + bmi)
+                .setPositiveButton("OK", (dialogInterface, i) -> {
+                    editHeight.setText("");
+                    editWeight.setText("");
+                }).show();
     }
 }
